bbb77cee476a9b7d96a8e2708a2e8f8512dae542
skgilmore <skg.gilmore@gmail.com>
2021-01-26 12:12:55 -0600

    can target single animal

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,13 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "./Animal.css"
 
 export const AnimalCard = ({ animal, owner, location }) => (
     <section className="animal">
         <h2>Animals:</h2>
+        <Link to={`/animals/detail/${animal.id}`}>
+          { animal.name }
+        </Link>
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal_name">{animal.breed}</div>
-        <div className="location__address">{animal.location.name}</div>   
-        <div className="customer__name">{owner.name}</div>
     </section>
 )
 
---
+++ src/components/animal/AnimalDetail.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react"
+import { AnimalContext } from "./AnimalProvider"
+import "./Animal.css"
+import { useParams, useHistory } from "react-router-dom"
+
+export const AnimalDetail = () => {
+  const { getAnimalById } = useContext(AnimalContext)
+
+	const [animal, setAnimal] = useState({})
+
+    const {animalId} = useParams();
+    // how youre accesing the id you are telling application view to go get
+
+  useEffect(() => {
+    console.log("useEffect", animalId)
+    getAnimalById(animalId)
+    .then((response) => {
+      setAnimal(response)
+    })
+    }, [])
+
+  return (
+    <section className="animal">
+      <h3 className="animal__name">{animal.name}</h3>
+      <div className="animal__breed">{animal.breed}</div>
+      {/* What's up with the question mark???? it allows the response to not exisit and
+      not break the app. show it if its there if its not there just leave blank*/}
+      <div className="animal__location">Location: {animal.location?.name}</div>
+      <div className="animal__owner">Customer: {animal.customer?.name}</div>
+    </section>
+  )
+}
